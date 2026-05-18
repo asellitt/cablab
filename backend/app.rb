e4e4d5cd0b58c5711f16683694cf826a4889860f
Anthony Sellitti <1116257+asellitt@users.noreply.github.com>
2026-05-18 15:26:45 +1000

    bypass host auth completely. lets see if this works

--- backend/app.rb
+++ backend/app.rb
@@ -17,7 +17,7 @@ class CablabApp < Sinatra::Base
     end
   end
 
-  set :protection, except: [:http_origin, :host_authorization]
+  set :host_authorization, allow_if: ->(_env) {true}
   set :show_exceptions, false
   set :raise_errors,    true
   set :data_file, '/data/topology.yaml'
